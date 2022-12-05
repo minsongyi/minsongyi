7ab35de9926f61bb9fcaa7547dda966a152d6768
minsongyi <alyssamchi0520@gmail.com>
2022-12-05 21:55:20 +0900

    [제귀] dfs
    https://www.acmicpc.net/problem/4963

---
+++ 4963.py
@@ -0,0 +1,11 @@
+while True:
+    m,n = [*map(int, input().split())] 
+    if m==0 and n==0:
+        break
+    L=[]
+    for i in range(0,n):
+        a= input()
+        L.append(a)
+    
+    
+        
