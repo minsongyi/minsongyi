71e93183755be4d2a114311af942b8edaf70dec4
minsongyi <alyssamchi0520@gmail.com>
2022-12-05 21:45:16 +0900

    [제귀] dfs
    https://www.acmicpc.net/problem/1012

--- 유기농배추.py
+++ 유기농배추.py
@@ -1,3 +1,6 @@
+import sys
+sys.setrecursionlimit(25000)
+
 t= int(input())
 for _ in range(t):
     L=[]
@@ -10,12 +13,30 @@ for _ in range(t):
         a,b = [*map(int, input().split())] 
         L[b][a]=1
         
+    dx=[0,0,1,-1]
+    dy=[1,-1,0,0]
+    def out_of_bound(y,x):
+        if y<0 or y>=n:
+            return True
+        if x<0 or x>=m:
+            return True
+        return False
+    
     def rec(y,x):
+        for d in range(0,4):
+            ny=y+dy[d]
+            nx=x+dx[d]
+            if out_of_bound(ny,nx)==True:
+                continue
+            if L[ny][nx]==1:
+                L[ny][nx]=0
+                rec(ny,nx)
         return
     worm=0    
     for i in range(0,n):
         for j in range(0,m):
             if L[i][j]==1:
+                L[i][j]=0
                 worm+=1
                 rec(i,j)
         
