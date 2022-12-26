230827950a05a29dc5a537c7a62b6d4dd199e099
minsongyi <alyssamchi0520@gmail.com>
2022-12-26 21:09:41 +0900

    [수학] 14912.py  https://www.acmicpc.net/problem/14912 -> 방법은 맞았는데 안 돌아감
    하나의 숫자의 모든 자리수 숫자를 작은 자리수 순서를 알아내는 방법
    
    (a//1)%10
    (a//10)%10
    (a//100)%10
    (a//1000)%10
    ...
    (a//(10**s))가 0이될때 멈추기

---
+++ 14912.py
@@ -0,0 +1,21 @@
+n,d =[*map(int, input().split())] 
+M=[0]*10
+s=0
+for i in range(1,n+1):
+    while True:
+        if (i//(10**s))==0: # 여기가 문제인것같음
+            break
+        else:
+            print(i,(i//(10**s))%10)
+            M[(i//(10**s))%10]+=1
+            s+=1
+
+print(M[d])
+print(M)
+        
+    
+'''
+(a//1000)%10
+(a//100)%10
+(a//10)%10
+'''
