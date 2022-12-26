0903f08c2dc475bf1769de265e2f9d7a00657978
minsongyi <alyssamchi0520@gmail.com>
2022-12-26 21:15:37 +0900

    [수학] 14912.py  초기화 실수 https://www.acmicpc.net/problem/14912
    -> 10의 거듭제곱을 숫자마다 초기화를 했어야했느데 안함

--- 14912.py
+++ 14912.py
@@ -2,16 +2,17 @@ n,d =[*map(int, input().split())]
 M=[0]*10
 s=0
 for i in range(1,n+1):
+    s=0
     while True:
         if (i//(10**s))==0: # 여기가 문제인것같음
             break
         else:
-            print(i,(i//(10**s))%10)
+            #print(i,(i//(10**s))%10)
             M[(i//(10**s))%10]+=1
             s+=1
 
 print(M[d])
-print(M)
+#print(M)
         
     
 '''
